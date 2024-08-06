5af61c89861c081129d8446c28a882e057ba9444
lounas <lounasidjr@gmail.com>
2024-08-06 01:01:25 +0100

    the code continue

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
+
+
+let humanScore = 0;
+let computerScore = 0; 
+
+
+
 function getComputerChoice(){
     const computerChoice = Math.floor(Math.random()*3);
     switch (computerChoice) {
         case 0:
-         console.log("rock");
+         console.log("computer choose a rock");
+         return rock ;
          break;
         case 1:
-        console.log("paper");
+        console.log("computer choose a paper");
+         return paper ;
          break;
         case 2:
-            console.log("Scissors");
+            console.log("computer choose a Scissor");
+            return scissor ;
          break;
     }
 };
 function getHumanChoice(){
 const validCHoice = ['rock','paper','scissor']; 
 while(true){
-    const humainChoice = prompt("Enter 'rock' or 'paper'or 'scissor'");
+    const humainChoice = prompt("Enter 'rock' or 'paper'or 'scissor'").toLowerCase;
     if(validCHoice.includes(humainChoice)){
         console.log("you choose : "+ humainChoice);
     }else{
         console.log("invalid choice.Please try again");
     }
 }
+};
+const playRound =(humainChoice,computerChoice)=>{
+ 
 }
 
-// getComputerChoice();
-getHumanChoice();
+const computerChoice =getComputerChoice();
+const humainChoice =getHumanChoice();
